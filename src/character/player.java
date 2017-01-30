8c202dbb5738565b40617cd9d9867eb78bae0da3
JimAlexBerger <alexandergundersen@yaho.no>
2017-01-30 23:08:57 +0100

    added update position

--- src/character/player.java
+++ src/character/player.java
@@ -1,5 +1,27 @@
 package character;
 
-public class player {
+import java.awt.event.KeyEvent;
 
+public class player extends Character {
+	
+	private double dx = 0;
+	private double dy = 0;
+	
+	double forceX = 0;
+	double forceY = 0;
+	
+	public void update(){
+		this.dx += forceX;
+		this.dy += forceY;
+		this.x += dx;
+		this.y += dy;
+		this.forceX = 0;
+		this.forceY = 0;
+		this.dx *= 0.9;
+		this.dy *= 0.9;
+	}
+	
+	void keyReleased(KeyEvent e){
+		System.out.println(e);
+	}
 }
--- src/launcher/Painter.java
+++ src/launcher/Painter.java
@@ -1,5 +1,11 @@
 package launcher;
 
-public class Painter {
+import java.awt.Graphics2D;
 
+public class Painter {
+	public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+
+        
+    }
 }
