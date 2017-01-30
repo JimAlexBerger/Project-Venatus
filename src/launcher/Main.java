28289b279383c717dba78988fd9cf3aa51e9a78b
JimAlexBerger <alexandergundersen@yaho.no>
2017-01-30 22:20:16 +0100

    Setup JFrame
    Added MainFrame and Main, main starts MainFrame...

---
+++ src/launcher/Main.java
@@ -0,0 +1,13 @@
+package launcher;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+	}
+
+}
---
+++ src/launcher/MainFrame.java
@@ -0,0 +1,31 @@
+package launcher;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
